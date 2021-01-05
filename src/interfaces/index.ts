f2b2f7b1c808cbc24468d7f6067ee0cc0296fad7
Andre <itstheandre@gmail.com>
2021-01-05 15:26:47 +0100

    feat(escape chars): add a way for users to be able to escape characters in their templates
    this is important because it can be an ovverride (in short term) for the chaning of a wrapper

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,3 @@
-/**
- * Hello
- */
 export interface ITemplateReader {
   /**
    * The `str` is the template that will be changed
@@ -18,10 +15,16 @@ export interface ITemplateReader {
   wrapper?: string;
   /**
    * @default false
+   * @description for every character you wish to escape, you must always pass a double slash `\\` between the characters you wish to escape.
+   * Until i figure out how to add on the two sides of the wrapper, please remember to always add two double slashes to the wrapper
+   * 👇 for an example
    * if you wish to escape the `\\`
    * @example
    * escape: true
-   * itsTheTemplator({str:"{\\{name}\\}"}, vars:{name:"André", escape: true}) = `{{name}}`
+   * itsTheTemplator({
+   *  str:"{\\{name}\\}"},
+   *  vars:{name:"André", escape: true})
+   * =  `{{name}}`
    */
   escape?: boolean;
 }
